75824eb83cbef84fe00d20fffba88330614a59d2
Leonardo de Camargo <leonardo4215@live.com>
2024-04-16 12:44:18 -0400

    Final version

---
+++ Controllers/LastAssignmentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment3.Controllers
+{
+    public class LastAssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Models/Plane.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class Plane
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+    }
+}
